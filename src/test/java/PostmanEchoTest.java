e4bbc66faf07960870b9fa35a9fdf24cb395fc86
Vitaliy <Groteckon@mail.ru>
2023-07-16 23:00:38 +0300

    add PostmanEchoTest

---
+++ src/test/java/PostmanEchoTest.java
@@ -0,0 +1,27 @@
+import io.restassured.matcher.ResponseAwareMatcher;
+import io.restassured.response.Response;
+import org.junit.jupiter.api.Test;
+
+import static io.restassured.RestAssured.given;
+import static org.hamcrest.Matchers.equalTo;
+
+public class PostmanEchoTest {
+
+    @Test
+     void shouldReturnData () {
+
+
+        given()
+                .baseUri("https://postman-echo.com")
+                .contentType("text/plain; charset=UTF-8")
+                .body("Солнечно")
+
+                .when()
+                .post("/post")
+
+                .then()
+                .statusCode(200)
+                .body("data", equalTo("Sun"));
+    }
+
+}
